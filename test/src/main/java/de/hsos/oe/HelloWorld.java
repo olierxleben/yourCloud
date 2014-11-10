a83912510f72e283bf39ce4b18eab4de9d7d255f
Oliver Erxleben <oliver.erxleben@me.com>
2014-11-10 15:13:58 +0100

    added gson as dependency for test, called it via HelloWorld. Example shows usage of gradle.

--- test/src/main/java/de/hsos/oe/HelloWorld.java
+++ test/src/main/java/de/hsos/oe/HelloWorld.java
@@ -1,10 +1,14 @@
 package de.hsos.oe;
 
+import com.google.gson.Gson;
+
 public class HelloWorld {
 
     public static void main(String[ ] args)
     {
-        // Ausgabe Hello World!
-        System.out.println("Hello World!");
+        Gson g = new Gson();
+        String json = g.toJson(123);
+
+        System.out.println("Hello " + json);
     }
 }
